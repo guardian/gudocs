a5c31275962ba30a6004c312519e6e41525120a4
Matt Osborn <matthew.osborn@theguardian.com>
2015-07-31 16:58:05 +0100

    sass and fonts

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+
+module.exports = function(grunt) {
+
+    require('jit-grunt')(grunt);
+
+    grunt.initConfig({
+
+        watch: {
+            css: {
+                files: ['src/css/**/*'],
+                tasks: ['sass'],
+            }
+        },
+
+        clean: {
+            build: ['build']
+        },
+
+        nodemon: {
+            dev: {
+                script: './src/cli.js',
+                options: {
+                    exec: ['babel-node --harmony'],
+                    env: { PORT: '4000' }
+                }
+            }
+        },
+
+        sass: {
+            options: {
+                sourceMap: true
+            },
+            interactive: {
+                files: {
+                    'build/main.css': 'src/css/main.scss'
+                }
+            }
+        },
+
+        concurrent: {
+            www: ['nodemon', 'watch'],
+            options: {
+                logConcurrentOutput: true
+            }
+        }
+    });
+
+    grunt.registerTask('build', ['clean', 'sass'])
+    grunt.registerTask('default', ['build', 'concurrent:www']);
+}
