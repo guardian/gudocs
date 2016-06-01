c3f23a49381f639c22b93c894f5b7b31ed35d04b
Will Franklin <will.franklin@guardian.co.uk>
2016-06-01 14:28:14 +0100

    Simplify auth passing

--- src/drive.js
+++ src/drive.js
@@ -1,16 +1,16 @@
-import { jwtClient } from './auth.js'
+import { jwtClient as auth } from './auth.js'
 import denodeify from 'denodeify'
 import google from 'googleapis'
 
 var drive = google.drive('v2');
 var sheets = google.sheets('v4');
 
-const jwtAuthorize = denodeify(jwtClient.authorize.bind(jwtClient));
+const jwtAuthorize = denodeify(auth.authorize.bind(auth));
 const listPermissions = denodeify(drive.permissions.list);
 const listChanges = denodeify(drive.changes.list);
 
 async function fetchAllChanges(pageToken = undefined) {
-    var options = Object.assign({'auth': jwtClient, 'maxResults': 1000}, pageToken ? {pageToken} : {});
+    var options = Object.assign({auth, 'maxResults': 1000}, pageToken ? {pageToken} : {});
     var page = await listChanges(options);
 
     var largestChangeId, items;
@@ -30,11 +30,11 @@ export default {
     fetchAllChanges,
 
     fetchRecentChanges(startChangeId) {
-        return listChanges({'auth': jwtClient, 'maxResults': 25, startChangeId});
+        return listChanges({auth, startChangeId, 'maxResults': 25});
     },
 
     fetchFilePermissions(fileId) {
-        return listPermissions({'auth': jwtClient, fileId});
+        return listPermissions({auth, fileId});
     },
 
     // TODO: deprecate in favour of googleapis
