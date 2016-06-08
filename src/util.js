2758eff49813e5730cf255e687f7a7cf51ed3dc4
Will Franklin <will.franklin@guardian.co.uk>
2016-06-08 10:52:32 +0100

    Add notification

--- src/util.js
+++ src/util.js
@@ -1,3 +1,8 @@
+import AWS from 'aws-sdk'
+import denodeify from 'denodeify'
+import config from '../gu.json' // hack because koa-gu doesn't load config until init
+import gu from 'koa-gu'
+
 export function delay(ms, then) {
     var interval;
     var promise = new Promise(resolve => interval = setTimeout(resolve, ms)).then(then);
@@ -5,4 +10,16 @@ export function delay(ms, then) {
         cancel() { clearTimeout(interval); },
         promise
     };
+}
+
+var credentials = new AWS.SharedIniFileCredentials(config.aws_profile);
+AWS.config.region = 'eu-west-1';
+AWS.config.credentials = credentials;
+var sns = new AWS.SNS({'params': {'TopicArn': config.snsErrors}});
+var snsPublish = denodeify(sns.publish.bind(sns));
+
+export function notify(subject, message) {
+    return snsPublish({'Subject': subject, 'Message': message}).catch(err => {
+        gu.log.error('Failed to send notification', err);
+    });
 }
