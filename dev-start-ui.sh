6c89e4727932d7cc17bab42f38c0aada6b03d889
Anna <ajwl@users.noreply.github.com>
2020-04-20 14:36:20 +0100

    Revert "Revert "Fix parsing bug, update dependencies & docs""

--- dev-start-ui.sh
+++ dev-start-ui.sh
@@ -5,4 +5,12 @@ export GU_sns_errors="not-used"
 export GU_dbkey="gudocs-dev"
 export GU_require_domain_permissions="dev-guardian.co.uk"
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+nvm install
+
+# run redis server in the background
+redis-server &
+
 npm run dev
