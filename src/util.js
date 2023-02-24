2b232db6b45e4e4ca27a653483223af5280357b0
akash1810 <akash1810@users.noreply.github.com>
2023-02-24 09:48:57 +0000

    feat!: Remove SNS notification on error
    Added in #12, a notification would be sent to SNS when the doc tool failed.
    
    The SNS topic has one subscriber - email, to a colleague who left a few years ago.
    
    In this change, we remove this error notification as we're never receiving the emails.
    The errors are still shown in the log page.

--- src/util.js
+++ src/util.js
@@ -1,8 +1,3 @@
-import AWS from 'aws-sdk'
-import denodeify from 'denodeify'
-import config from '../gu.json' // hack because koa-gu doesn't load config until init
-import gu from 'koa-gu'
-
 export function delay(ms, then) {
     var interval;
     var promise = new Promise(resolve => interval = setTimeout(resolve, ms)).then(then);
@@ -10,12 +5,4 @@ export function delay(ms, then) {
         cancel() { clearTimeout(interval); },
         promise
     };
-}
-
-export function notify(subject, message, topicArn) {
-    var sns = new AWS.SNS({'params': {'TopicArn': topicArn}});
-    var snsPublish = denodeify(sns.publish.bind(sns));
-    return snsPublish({'Subject': subject, 'Message': message}).catch(err => {
-        gu.log.error('Failed to send notification', err);
-    });
 }
